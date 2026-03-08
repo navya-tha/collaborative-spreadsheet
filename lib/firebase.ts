5c71d6fb0c728e623823978cceb670c696d97285
Navya <navyatha.23bce9189@vitapstudent.ac.in>
2026-03-08 15:50:33 +0530

    Use environment variables for Firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,18 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCRIxbu7L47ERfQKIeBYy3ScpyDIIMOrlg",
-  authDomain: "collaborativespreadsheet-31771.firebaseapp.com",
-  projectId: "collaborativespreadsheet-31771",
-  storageBucket: "collaborativespreadsheet-31771.firebasestorage.app",
-  messagingSenderId: "332003997436",
-  appId: "1:332003997436:web:1a1a8977e201f6de49c979",
-  measurementId: "G-Q8DP5NT4WF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
